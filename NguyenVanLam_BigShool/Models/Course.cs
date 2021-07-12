dde47e6d2d0d21c9a4ffd268fd639b7d13fde4bc
lam <lam719123@gmail.com>
2021-07-12 07:32:59 +0700

    1

--- NguyenVanLam_BigShool/Models/Course.cs
+++ NguyenVanLam_BigShool/Models/Course.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Build.Framework.XamlTypes;
+﻿
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
---
+++ NguyenVanLam_BigShool/ViewModel/FollowingViewModel.cs
@@ -0,0 +1,14 @@
+﻿using NguyenVanLam_BigShool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NguyenVanLam_BigShool.ViewModel
+{
+    public class FollowingViewModel
+    {
+        public IEnumerable<ApplicationUser> Followings { get; set; }
+        public bool ShowAction { get; set; }
+    }
+}
